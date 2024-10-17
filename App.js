dbcc7a848fc71a5c51689438b78dcc7065619b3a
NUR HAFIZAH RAMLI <hafizah.rmli@gmail.com>
2024-10-18 02:00:05 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/navigations/AppNavigation';
+import firebase from '@react-native-firebase/app';
+
+export default function App() {
+  return (
+     <AppContainer />
+  );
+}
